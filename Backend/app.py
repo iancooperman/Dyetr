00245dcb75cdba87b23f2a8e5a009f3522d15e18
isaiahar <44944120+isaiahar@users.noreply.github.com>
2021-02-28 18:08:35 -0800

    App.py now handles ATE & LIKES endpoints
    api/v1/food_eaten
    api/v1/food_liked

--- Backend/app.py
+++ Backend/app.py
@@ -58,6 +58,81 @@ def post_food():
     _db_driver.create_food_tem(item['id'], item['name'], item['calories'], item['carbohydrates'], item['fat'], item['protein'])
     return '', 201
 
+# URL params should be: /food_eaten?user_id=......&food_id=......&meal_type=......
+@app.route('/api/v1/food_eaten', methods=["GET", "POST"])
+def food_eaten():
+    if request.method() == "GET":
+        if request.args:
+
+            if "user_id" in args:
+                
+                user_id = args["user_id"]
+
+                eat_time = str(datetime.now())
+                all_food_eaten = _db_driver.find_food_eaten_by_user(user_id)
+                
+                if all_food_eaten:
+                    return (all_food_eaten, 200)
+                else:
+                    return '', 404
+    elif request.method() == "POST":
+        if request.args:
+
+            if "user_id" in args:
+                if "food_id" in args:
+                    if "meal_type" in args:
+
+                        user_id = args["user_id"]
+                        food_id = args["food_id"]
+                        meal_type = args["meal_type"]
+
+                        eat_time = str(datetime.now())
+                        _db_driver.create_ate_relationship(user_id, food_id, eat_time, meal_type)
+                        
+                        return '', 201
+                    
+# URL params should be: /food_eaten?user_id=......&food_id=......
+@app.route('/api/v1/food_liked', methods=["GET", "POST", "DELETE"])
+def food_liked():
+    if request.method() == "GET":
+        if request.args:
+
+            if "user_id" in args:
+                
+                user_id = args["user_id"]
+
+                eat_time = str(datetime.now())
+                all_food_liked = _db_driver.find_food_liked_by_user(user_id)
+                
+                if all_food_liked:
+                    return (all_food_liked, 200)
+                else:
+                    return '', 404
+    elif request.method() == "POST":
+        if request.args:
+
+            if "user_id" in args:
+                if "food_id" in args:
+                    
+                    user_id = args["user_id"]
+                    food_id = args["food_id"]
+
+                    _db_driver.create_likes_relationship(self, user_id: str, food_id: str)
+                    
+                    return '', 201
+    elif request.method() == "DELETE":
+        if request.args:
+
+            if "user_id" in args:
+                if "food_id" in args:
+
+                    user_id = args["user_id"]
+                    food_id = args["food_id"]
+
+                    _db_driver.delete_likes_relationship(user_id, food_id)
+                    
+                    return '', 201
+
 db_driver.close()
 
 if __name__ == '__main__':
