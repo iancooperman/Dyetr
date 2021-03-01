fae5bb6d73929ae829b3981d1405e7c7e54d26d0
isaiahar <44944120+isaiahar@users.noreply.github.com>
2021-02-28 17:51:16 -0800

    Update db.py to handle ATE and LIKES relationships
    ATE relationship: GET a list of foods that a user has eaten, POST a new food that a user has eaten.
    LIKES relationship: GET a list of foods that a user has liked, POST a new food that a user has liked, DELETE an existing LIKES relationship between a user and a food

--- Backend/db.py
+++ Backend/db.py
@@ -22,3 +22,76 @@ class db:
         query = 'CREATE (f:food {id: $id, name: $name, calories: $calories, carbohydrates: $carbohydrates,  fat: $fat, protein: $protein})'
         with self._db_driver.session() as session:
             session.run(query, id = food_id, name = item_name, calories = calories, carbohydrates = carbs, fat = fat, protein = protein)
+            
+    """RETURNS: list of food items eaten by a provided user"""
+    def find_food_eaten_by_user(self, user_id: str):
+        all_food_eaten = (f"MATCH (u: user), (f: food) WHERE u.id = {user_id} AND (u)-[:ATE]->(f) RETURN collect(f)")
+
+        # Above query (beautified):
+        # MATCH (u: User), (f: food)
+        # WHERE u.id = {user_id} AND (u)-[:ATE]->(f)
+        # RETURN collect(f)
+
+        with self._db_driver.session() as session:
+            result = session.run(all_food_eaten)
+
+        return result, 201
+    
+    """RETURNS: None
+       CREATES: New ATE relationship between provided user and food"""
+    def create_ate_relationship(self, user_id: str, food_id: str, eat_time: str, meal_type: str):
+        new_food_eaten = (f"MATCH (u: user), (f: food) WHERE u.id = {user_id} AND f.id = {food_id} CREATE (u)-[:ATE {{timestamp: {eat_time}, meal: {meal_type}}}]->(f)")
+
+        # Above query (beautified):
+        # MATCH (u: User), (f: food)
+        # WHERE u.id = {user_id} AND f.id = {food_id}
+        # CREATE (u)-[:ATE {{timestamp: {eat_time}, meal: {meal_type}}}]->(f)
+
+        with db_driver.session() as session:
+            session.run(new_food_eaten)
+
+        return '', 201
+    
+    """RETURNS: list of food items liked by a provided user"""
+    def find_food_liked_by_user(self, user_id: str):
+        all_food_liked = (f"MATCH (u: user), (f: food) WHERE u.id = {user_id} AND (u)-[:LIKES]->(f) RETURN collect(f)")
+
+        # Above query (beautified):
+        # MATCH (u: User), (f: food)
+        # WHERE u.id = {user_id} AND (u)-[:LIKES]->(f)
+        # RETURN collect(f)
+
+        with db_driver.session() as session:
+            result = session.run(all_food_liked)
+
+        return result, 201
+    
+    """RETURNS: None
+       CREATES: New LIKES relationship between provided user and food"""
+    def create_likes_relationship(self, user_id: str, food_id: str):
+        new_food_liked = (f"MATCH (u: user), (f: food) WHERE u.id = {user_id} AND f.id = {food_id} CREATE (u)-[:LIKES]->(f)")
+
+        # Above query (beautified):
+        # MATCH (u: User), (f: food)
+        # WHERE u.id = {user_id} AND f.id = {food_id}
+        # CREATE (u)-[:LIKES]->(f)
+
+        with db_driver.session() as session:
+            session.run(new_food_liked)
+
+        return '', 201
+    
+    """RETURNS: None
+       DELETES: An existing LIKES relationship between provided user and food"""
+    def delete_likes_relationship(self, user_id: str, food_id: str):
+        food_deleted = (f"MATCH (u: User)-[r:LIKES]->(f: food) WHERE u.id = {user_id} AND f.id = {food_id} DELETE r")
+
+        # Above query (beautified):
+        # MATCH (u: User)-[r:LIKES]->(f: food)
+        # WHERE u.id = {user_id} AND f.id = {food_id}
+        # DELETE r
+
+        with db_driver.session() as session:
+            session.run(food_deleted)
+
+        return '', 201
