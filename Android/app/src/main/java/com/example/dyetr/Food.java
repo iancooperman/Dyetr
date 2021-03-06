f5fa60c33174be1e8742b489d40af09f35eeba86
Ian Cooperman <i.perry.leonard.c@gmail.com>
2021-03-06 13:02:30 -0800

    Created Food class

---
+++ Android/app/src/main/java/com/example/dyetr/Food.java
@@ -0,0 +1,44 @@
+package com.example.dyetr;
+
+public class Food {
+    private String id;
+    private String name;
+    private double calories;
+    private double carbohydrates;
+    private double protein;
+    private double fats;
+
+
+    Food(String id, String name, double calories, double carbohydrates, double protein, double fats) {
+        this.id = id;
+        this.name = name;
+        this.calories = calories;
+        this.carbohydrates = carbohydrates;
+        this.protein = protein;
+        this.fats = fats;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCalories() {
+        return calories;
+    }
+
+    public double getCarbohydrates() {
+        return carbohydrates;
+    }
+
+    public double getProtein() {
+        return protein;
+    }
+
+    public double getFats() {
+        return fats;
+    }
+}
