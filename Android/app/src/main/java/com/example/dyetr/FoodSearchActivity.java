2944f90db80c295f846d77e259b88b149a2725d1
Ian Cooperman <i.perry.leonard.c@gmail.com>
2021-03-07 13:56:52 -0800

    Relearning the basics :)

--- Android/app/src/main/java/com/example/dyetr/FoodSearchActivity.java
+++ Android/app/src/main/java/com/example/dyetr/FoodSearchActivity.java
@@ -1,16 +1,45 @@
 package com.example.dyetr;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class FoodSearchActivity extends AppCompatActivity {
+
+    private EditText searchPlainText;
+    private Button searchButton;
+    private TextView weRecommendTextView;
+    private ListView foodListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_search);
         Log.i(this.getLocalClassName(), "Activity created.");
+
+        searchPlainText = (EditText) findViewById(R.id.searchPlainText);
+        searchButton = (Button) findViewById(R.id.searchButton);
+        weRecommendTextView = (TextView) findViewById(R.id.weRecommendTextView);
+        foodListView = (ListView) findViewById(R.id.foodListView);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("click", "here");
+                Context context = getApplicationContext();
+                String text = searchPlainText.getText().toString();
+                Toast toast = Toast.makeText(context, text, Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
     }
 }
